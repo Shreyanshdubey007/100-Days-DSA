54e59b0d82b56a6cfff384106a162846cf66b8a3
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-03 21:10:03 +0530

    Find missing number in array (1-Based position)

---
+++ Day03/missinginArray01.c
@@ -0,0 +1,30 @@
+//You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive).
+//This array represents a permutation of the integers from 1 to n with one element missing.
+//Your task is to identify and return the missing element.
+
+#include <stdio.h>
+
+int missingNum(int *arr, int size) {
+    long long expectedSum, actualSum = 0;
+
+    // Numbers should be from 1 to size+1
+    expectedSum = (long long)(size + 1) * (size + 2) / 2;
+
+    for (int i = 0; i < size; i++) {
+        actualSum += arr[i];
+    }
+
+    return (int)(expectedSum - actualSum);
+}
+
+int main() {
+
+    int arr[] = {1, 2, 3, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int missing = missingNum(arr, size);
+
+    printf("Missing number is: %d\n", missing);
+
+    return 0;
+}
