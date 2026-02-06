fd0d240345ef09d850000090f695e03a69856e28
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-06 21:34:08 +0530

    Removes Duplicate Elements from Array

---
+++ Day06/removeDuplicate.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, k = 1;  // Index for unique elements
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d terms of Array: ", n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    if(n == 0)
+        return 0;
+
+    for(int i = 1; i < n; i++){
+        if(arr[i] != arr[i - 1]){
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+
+    printf("Array: ");
+    for(int i = 0; i < k; i++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
