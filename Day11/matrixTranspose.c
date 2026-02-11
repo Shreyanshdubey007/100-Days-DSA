17928a27d10b3abc17bbd1aa8ec32612ccbccdac
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-11 22:14:04 +0530

    Add Transpose of Matrix

---
+++ Day11/matrixTranspose.c
@@ -0,0 +1,36 @@
+//Given a 2D integer array matrix, return the transpose of matrix.
+
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+    int transpose[n][m];
+
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
