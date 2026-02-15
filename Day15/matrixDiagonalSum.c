4c364fce8255c134375edd3debc196de474c79f9
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-15 13:09:30 +0530

    Add Problem Statement

--- Day15/matrixDiagonalSum.c
+++ Day15/matrixDiagonalSum.c
@@ -1,3 +1,5 @@
+//Problem: Given a matrix, calculate the sum of its primary diagonal elements. The primary diagonal consists of elements where row index equals column index.
+
 #include <stdio.h>
 
 int main() {
