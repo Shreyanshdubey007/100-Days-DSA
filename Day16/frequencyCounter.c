38f245ed6f4c210dc8c2c8f0e33783cef82dca1f
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-16 23:51:49 +0530

    Add Frequency Counter

---
+++ Day16/frequencyCounter.c
@@ -0,0 +1,42 @@
+//Problem: Given an array of integers, count the frequency of each distinct element and print the result.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements of array: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int visited[n];
+
+    for(int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d:%d\n", arr[i], count);
+    }
+
+    return 0;
+}
