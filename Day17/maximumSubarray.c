0a3a67f7b8151d881c8d89bf2753d828e7ef7749
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-17 23:59:24 +0530

    Add Maximum Subarray

---
+++ Day17/maximumSubarray.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxSubArray(int* nums, int numsSize) {
+    int currentSum = nums[0];
+    int maxSum = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        if (currentSum < 0)
+            currentSum = nums[i];
+        else
+            currentSum += nums[i];
+
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int* nums = (int*)malloc(n * sizeof(int));
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int result = maxSubArray(nums, n);
+
+    printf("Maximum Subarray Sum: %d\n", result);
+
+    free(nums);
+
+    return 0;
+}
