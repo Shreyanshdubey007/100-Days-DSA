4c002b1afd654cbc0ca98fd75cbc18451d0ab396
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-02-21 20:47:08 +0530

    Add Middle of Linked List

---
+++ Day21/middleLinkedList.c
@@ -0,0 +1,54 @@
+//Given the head of a singly linked list, return the middle node of the linked list.
+//If there are two middle nodes, return the second middle node.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* middleNode(struct ListNode* head) {
+    if (head == NULL) return NULL;
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    struct ListNode *head = NULL, *temp = NULL, *newNode;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = x;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    struct ListNode *mid = middleNode(head);
+
+    while (mid != NULL) {
+        printf("%d ", mid->val);
+        mid = mid->next;
+    }
+
+    return 0;
+}
