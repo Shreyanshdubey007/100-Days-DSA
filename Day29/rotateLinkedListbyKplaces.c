0efab91980896c9d55571496e569d4b0348f4a33
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-03-01 15:39:10 +0530

    Add Rotate Linked List Right by k Places

---
+++ Day29/rotateLinkedListbyKplaces.c
@@ -0,0 +1,80 @@
+//Problem: Rotate Linked List Right by k Places - Implement using linked list with dynamic memory allocation.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int n = 1;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        n++;
+    }
+
+    temp->next = head;
+
+    k = k % n;
+    int steps = n - k;
+
+    struct Node* newTail = head;
+    for (int i = 1; i < steps; i++) {
+        newTail = newTail->next;
+    }
+
+    struct Node* newHead = newTail->next;
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+int main() {
+    int n, k, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("List is empty\n");
+        return 0;
+    }
+
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
