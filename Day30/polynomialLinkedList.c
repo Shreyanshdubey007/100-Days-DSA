d98750021f990f4bf7bbf051c42bc68339b35caf
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-03-02 19:44:05 +0530

    Add Polynomial Using Linked List

---
+++ Day30/polynomialLinkedList.c
@@ -0,0 +1,58 @@
+//Problem: Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+int main() {
+    int n, c, e;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("No polynomial\n");
+        return 0;
+    }
+
+    printf("Enter coefficient and exponent:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &c, &e);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->coeff = c;
+        newNode->exp = e;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    temp = head;
+    while (temp != NULL) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
