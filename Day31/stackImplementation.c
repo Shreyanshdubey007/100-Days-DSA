7b91426f7878e4ff0007123b384037e691578763
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-03-03 23:37:13 +0530

    Update Stack Implementation using Array

--- Day31/stackImplementation.c
+++ Day31/stackImplementation.c
@@ -1,6 +1,7 @@
 //Problem: Implement a stack data structure using an array with the following operations: push, pop, and display.
 
 #include <stdio.h>
+
 #define MAX 1000
 
 int stack[MAX];
@@ -12,10 +13,12 @@ void push(int val) {
 }
 
 void pop() {
-    if (top == -1)
+    if (top == -1) {
         printf("Stack Underflow\n");
-    else
+    } 
+    else {
         printf("Popped: %d\n", stack[top--]);
+    }
 }
 
 void display() {
@@ -24,26 +27,31 @@ void display() {
         return;
     }
     printf("Stack (top to bottom): ");
-    for (int i = top; i >= 0; i--)
+    for (int i = top; i >= 0; i--) {
         printf("%d ", stack[i]);
+    }
     printf("\n");
 }
 
 int main() {
     int n, op, val;
+
     printf("Enter number of operations: ");
     scanf("%d", &n);
-    
+
     while (n--) {
         printf("Enter operation (1=push, 2=pop, 3=display): ");
         scanf("%d", &op);
+
         if (op == 1) {
             printf("Enter value to push: ");
             scanf("%d", &val);
             push(val);
-        } else if (op == 2) {
+        } 
+        else if (op == 2) {
             pop();
-        } else if (op == 3) {
+        } 
+        else if (op == 3) {
             display();
         }
     }
