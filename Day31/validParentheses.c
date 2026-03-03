d43cbcc73215ba8fe350cb0505741bce60d119d0
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-03-03 23:42:11 +0530

    Update Valid Parentheses

--- Day31/validParentheses.c
+++ Day31/validParentheses.c
@@ -11,30 +11,43 @@ bool isValid(char* s) {
 
     for (int i = 0; i < n; i++) {
         char c = s[i];
+
         if (c == '(' || c == '{' || c == '[') {
             stack[++top] = c;
-        } else {
-            if (top == -1) return false;
+        } 
+        else {
+            if (top == -1) {
+                return false;
+            }
+
             char t = stack[top--];
-            if (c == ')' && t != '(') return false;
-            if (c == '}' && t != '{') return false;
-            if (c == ']' && t != '[') return false;
+
+            if (c == ')' && t != '(') {
+                return false;
+            }
+            if (c == '}' && t != '{') {
+                return false;
+            }
+            if (c == ']' && t != '[') {
+                return false;
+            }
         }
     }
     return top == -1;
 }
 
 int main() {
-
     char s[10001];
-    
+
     printf("Enter the string: ");
     scanf("%s", s);
 
-    if (isValid(s))
+    if (isValid(s)) {
         printf("Output: true\n");
-    else
+    } 
+    else {
         printf("Output: false\n");
+    }
 
     return 0;
 }
