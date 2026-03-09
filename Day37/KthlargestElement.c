15dc58e6c2659f6d1761ef20b7763c31b878d478
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-03-09 17:57:11 +0530

    Update Kth Largest Element in a Stream

--- Day37/KthlargestElement.c
+++ Day37/KthlargestElement.c
@@ -1,7 +1,6 @@
 //You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
 
 #include <stdio.h>
-
 #include <stdlib.h>
 
 typedef struct {
