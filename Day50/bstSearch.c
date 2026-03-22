d5a72f5d65cb12104c2317e060265f4bc0a485ef
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-03-22 16:04:52 +0530

    BST Search

---
+++ Day50/bstSearch.c
@@ -0,0 +1,106 @@
+//Problem: BST Search
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) {
+        return createNode(val);
+    }
+
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    }
+    else if (val > root->data) {
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+struct Node* search(struct Node* root, int val) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (val == root->data) {
+        return root;
+    }
+    else if (val < root->data) {
+        return search(root->left, val);
+    }
+    else {
+        return search(root->right, val);
+    }
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void freeTree(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    freeTree(root->left);
+    freeTree(root->right);
+    free(root);
+}
+
+int main() {
+    int n, val, key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    printf("Enter %d elements: ", n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    printf("Inorder traversal: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Enter value to search: ");
+    scanf("%d", &key);
+
+    struct Node* result = search(root, key);
+
+    if (result != NULL) {
+        printf("Found: %d\n", result->data);
+    }
+    else {
+        printf("Not found\n");
+    }
+
+    freeTree(root);
+
+    return 0;
+}
