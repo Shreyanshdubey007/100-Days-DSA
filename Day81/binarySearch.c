143c9f98a88eff17bf2b74cf9ffb6acd490ca597
Shreyansh Dubey <shreyanshdubey182@gmail.com>
2026-04-23 20:30:56 +0530

    Add Binary Search

---
+++ Day81/binarySearch.c
@@ -0,0 +1,36 @@
+//Given an array of integers nums which is sorted in ascending order, and an integer target,
+//write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &target);
+
+    int left = 0, right = n - 1, ans = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            ans = mid;
+            break;
+        }
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
+}
